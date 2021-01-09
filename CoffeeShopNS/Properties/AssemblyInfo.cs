89122f2ac518407ac0aded47790da24760b5d99e
sundashafeez00@gmail.com <sundashafeez00@gmail.com>
2021-01-09 13:44:49 +0500

    adding files

---
+++ CoffeeShopNS/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CoffeeShopNS")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CoffeeShopNS")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2a51db17-7aa3-42a3-aa7a-e254d752f3c4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CoffeeShopNS/UnitTest1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CoffeeShop;
+
+namespace CoffeShopNS
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void CoffePrice_CustomerHasNotEnoughMoney_UpdatesBalance()
+        {
+            double AmountPaid = 11.99;
+            double Amountdeducted = 4.55;
+            double change = 7.44;
+            Coffeshop account = new Coffeshop("MS. ", AmountPaid);
+
+            account.Debit(Amountdeducted);
+
+            double actual = account.Balance;
+            Assert.AreEqual(change, actual, 0.001, "Sorry, Amount not paid succesfully");
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void CoffePrice_CustomersAmountIsLessThanZero_ShouldThrowArgumentOutOfRange()
+        {
+            double AmountPaid = 11.99;
+            double AccountIsEmpty = -100.00;
+            Coffeshop account = new Coffeshop("MS. SUNDAS", AmountPaid);
+
+            account.Debit(AccountIsEmpty);
+
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void CoffePrice_TransactionNotSuccesfull_ShouldThrowArgumentOutOfRange()
+        {
+            double AmountPaid = 11.99;
+            double AccountIsEmpty = -100.00;
+            Coffeshop account = new Coffeshop("MS. SUNDAS", AmountPaid);
+
+            account.Debit(AccountIsEmpty);
+
+        }
+
+
+        [TestMethod]
+        public void CoffePrice_WhenAmountIsMoreThanBalance_ShouldThrowArgumentOutOfRange()
+        {
+            double AmountPaid = 11.99;
+            double ActualAmount = 20.0;
+            Coffeshop account = new Coffeshop("MS. SUNDASj", AmountPaid);
+
+            try
+            {
+                account.Debit(ActualAmount);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                StringAssert.Contains(e.Message, Coffeshop.DebitAmountExceedsBalanceMessage);
+            }
+        }
+    }
+}
+
